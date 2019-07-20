cfa56d82b58867386d02fee083106b10a8647708
BrentBauman86 <brentbauman86@gmail.com>
2019-07-20 10:35:59 -0700

    add redux store

--- strider-frontend/src/index.js
+++ strider-frontend/src/index.js
@@ -3,10 +3,28 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const users = () => []
+
+const reducer = combineReducers({
+    users 
+})
+
+const composeEnhancer = window._REDUX_DEVTOOLS_EXTENSIONS_COMPOSE_ || compose;
+
+const store = createStore(reducer, composeEnhancer(applyMiddleware(thunk)))
+
+ReactDOM.render(
+<Provider store={ store }>
+    <App />
+</Provider>,
+document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
