efb4641dc4e917c409e881071845d1253480cf48
BrentBauman86 <brentbauman86@gmail.com>
2019-07-22 04:11:52 -0700

    build input form validations

--- strider-frontend/src/components/Login.js
+++ strider-frontend/src/components/Login.js
@@ -2,18 +2,46 @@ import React from 'react'
 import { Field, reduxForm } from 'redux-form';
 
 class Login extends React.Component {
-    renderInput() {
-        return <input />;
+    renderInput({ input, label, meta }) {
+        console.log(meta);
+        return (
+          <div className="field">
+            <label>{label}</label>
+            <input {...input}/>
+            <div>{meta.error}</div>
+          </div>
+      );
     }
+
+    onSubmit(formValues) {
+        console.log(formValues);
+    }
+
     render() {
         return (
-            <form>
-                <Field name="name" component={this.renderInput}/>
-                <Field name="password" component={this.renderInput}/>
+            <form onSubmit={this.props.handleSubmit(this.onSubmit)}className="ui form">
+                <Field name="name" label="Enter Name" component={this.renderInput}/>
+                <Field name="password" label="Enter Password" component={this.renderInput}/>
+                <button className="ui button primary">Submit</button>
             </form>
         );
     }
 }
   
-export default reduxForm({form: 'login'})(Login); 
+const validate = formValues => {
+    console.log(formValues)
+    const errors = {};
+
+    if (!formValues.name) {
+        errors.name = "Please enter your name";
+    }
+
+    if (!formValues.password) {
+        errors.password = "Invalid password";    
+    }
+
+    return errors; 
+}
+
+export default reduxForm({form: 'login', validate})(Login); 
 
